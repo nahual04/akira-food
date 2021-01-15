4e2bfbf8815c9ff85500f203c31258d322ef8795
Robelci <nahual04@gmail.com>
2021-01-15 16:09:24 -0600

    start project with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      montse: ['Montserrat'],
+    },
+
+    extend: {
+      colors: {
+        blackAkira: '#222',
+        
+        rojoAkira: {
+          100: '#E54658',
+          200: '#C5221F',
+        },
+        
+        yellowAkira: {
+          100: '#F6B142',
+          200: '#FFCB00',
+        },
+
+        blueAkira: {
+          100: '#F0F0FA',
+        },
+
+        greenAkira: '#81B057',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
